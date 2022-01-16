9d346c3876e8e10cac89a37fc49130e2f85f523a
hwangjokim <hg7258@naver.com>
2022-01-17 05:05:04 +0900

    메소드 분화 및 코드 정리

--- src/Game.java
+++ src/Game.java
@@ -9,16 +9,21 @@ public class Game {
         int line = line_check.line_chk();
         String[][] words = new String[line][2];
         file.read_f(words,line);
+        sorter(words);
+        /*  for(int i=0;i<line;i++){
+            System.out.println(words[i][0]+" "+words[i][1]);
+        } //!!DEBUG CODE!!*/
+        return quiz(words,line);
+    }
+    public static void sorter(String[][] words){
         Arrays.sort(words, (o1, o2) -> {
             if(o1[0].contentEquals(o2[0]))
                 return o1[1].compareTo(o2[1]);
             else
                 return o1[0].compareTo(o2[0]);
         }); // 2차원 배열 정렬식 복붙함
-
-        /*  for(int i=0;i<line;i++){
-            System.out.println(words[i][0]+" "+words[i][1]);
-        } //!!DEBUG CODE!!*/
+    }
+    public static int quiz(String[][] words,int line){
         Scanner sc = new Scanner(System.in);
         int score=0;
         String answer;
